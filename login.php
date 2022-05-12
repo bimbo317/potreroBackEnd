9051a49c35e6d0242c21560ad0b9e78691d6f053
Bimbo317 <bimpo2002@hotmail.com>
2022-05-11 21:49:52 -0300

    Se agrega tabla usuarios y se modifica el Login

--- login.php
+++ login.php
@@ -1,14 +1,26 @@
 <?php
-$user =$_POST["user"];
-$pass= $_POST["password"];
-$usuario="bimbo";
-$contra="bimbo";
-if ($user==$usuario && $pass==$contra) {
-    header("location:index.html");
-} else {
-    include("login.html");
-    echo '<script>
+$user = $_POST["user"];
+$pass = $_POST["password"];
+
+// 1) Conexion y selección de base de datos
+$conexion = mysqli_connect("127.0.0.1", "root", "", null, "3366");
+mysqli_select_db($conexion, "tienda"); // esto lo podemos poner acá o mas abajo, no hay problema
+
+// 2) Preparar la orden SQL
+
+$consulta = 'SELECT * FROM usuario';
+
+// 3) Ejecutar la orden y obtenemos los registros
+$datos1 = mysqli_query($conexion, $consulta);
+
+while ($reg1 = mysqli_fetch_array($datos1)) {
+    $usuario = $reg1['user'];
+    $contra = $reg1['password'];
+    if ($user == $usuario && $pass == $contra) {
+        header("location:index.html");
+    }
+}
+include("login.html");
+echo '<script>
     document.getElementById("error").style.display = "block"
     </script>';
-}
-?>
