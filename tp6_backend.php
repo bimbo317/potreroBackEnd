ba1452e7b3e59f5ebd87cc38bfc619706559ed83
Bimbo317 <bimpo2002@hotmail.com>
2022-05-11 21:27:49 -0300

    Correccion TP6

--- tp6_backend.php
+++ tp6_backend.php
@@ -66,8 +66,16 @@
                                     <section>
                                         <div class="container">
                                             <div class="row">
-
                                                 <?php
+                                                echo '<div class="mt-2 subtitle-1">1. Ingresar los siguientes datos utilizando la interfaz gráfica de phpMyAdmin y almacenarlo en la 
+                                                base de datos anteriormente creada en la Tarea N°</div>';
+                                                echo '<div class="mt-2 subtitle-1"><div class="mt-2 ml-2 subtitle-1">
+                                                a. buzo, nike, xxl, 600</div>
+                                                </div>';
+                                                echo '<div class="mt-2 subtitle-1 pb-2"><div class="mt-2 ml-2 subtitle-1">
+                                                b. buzo, addidas, l, 400</div>
+                                                </div>';
+                                                echo '<img src="./img/tp06.jpg">';
                                                 // 1) Conexion y selección de base de datos
                                                 $conexion = mysqli_connect("127.0.0.1", "root", "", null, "3366");
                                                 mysqli_select_db($conexion, "tienda"); // esto lo podemos poner acá o mas abajo, no hay problema
@@ -77,19 +85,64 @@
                                                 $consulta = 'SELECT * FROM ropa';
 
                                                 // 3) Ejecutar la orden y obtenemos los registros
-                                                $datos = mysqli_query($conexion, $consulta);
+                                                $datos1 = mysqli_query($conexion, $consulta);
 
                                                 //  recorro todos los registros y genero una CARD PARA CADA UNA
-                                                while ($reg = mysqli_fetch_array($datos)) { ?>
-                                                    <div class="card col-sm-12 col-md-6 col-lg-3 m-1">
-                                                        <!-- <img class="card-img-top" src="data:image/jpg;base64, <?php echo base64_encode($reg['imagen']) ?>" alt="" width="100px" height="100px" )> -->
-                                                        <h3 class="card-title" style="width: 100%; font-size:25px;"><?php echo ucwords($reg['marca']) ?></h3>
-                                                        <span>$ <?php echo $reg['precio']; ?></span>
-
-                                                    </div>
-
-                                                <?php } ?>
-
+                                                // Ejercicio 2 parte a
+                                                echo '<div class="mt-2 subtitle-1">2. Mediante código PHP, consultar a la base de datos lo siguiente:
+                                                <div class="mt-2 ml-2 subtitle-1">
+                                                a. Listar todos los registros </div>
+                                                </div>';
+                                                echo '<div class="row ml-1">';
+                                                while ($reg1 = mysqli_fetch_array($datos1)) {
+                                                    echo '<div class="card col-sm-12 col-md-6 col-lg-3 m-1">';
+                                                    echo '<h3 class="card-title" style="width: 100%; font-size:25px;">' . ucwords($reg1['marca']) . '</h3>';
+                                                    //echo '<h3 class="card-title" style="width: 100%; font-size:25px;">'.ucwords($reg['marca']).'</h3>';
+                                                    echo '<span>$ ' . $reg1['precio'] . '</span>';
+                                                    echo '</div>';
+                                                }
+                                                echo '</div>';
+                                                // Ejercicio 2 parte b
+                                                echo '<div class="mt-2 subtitle-1">
+                                                <div class="mt-2 ml-2 subtitle-1">b. Listar todos los registros cuyo tipo de prenda sea ‘buzo’</div>
+                                                </div>';
+                                                $datos2 = mysqli_query($conexion, $consulta);
+                                                while ($reg2 = mysqli_fetch_array($datos2)) {
+                                                    if ($reg2['tipo_de_prenda'] == 'buzo') {
+                                                        echo '<div class="card col-sm-12 col-md-6 col-lg-3 m-1">';
+                                                        echo '<h3 class="card-title" style="width: 100%; font-size:25px;">' . ucwords($reg2['marca']) . '</h3>';
+                                                        echo '<span>$ ' . $reg2['precio'] . '</span>';
+                                                        echo '</div>';
+                                                    }
+                                                }
+                                                // Ejercicio 2 parte c
+                                                echo '<div class="mt-2 subtitle-1">
+                                                <div class="mt-2 ml-2 subtitle-1">c. Listar todos los registros cuya marca sea ‘nike’</div>
+                                                </div>';
+                                                $datos3 = mysqli_query($conexion, $consulta);
+                                                while ($reg3 = mysqli_fetch_array($datos3)) {
+                                                    if ($reg3['marca'] == 'nike') {
+                                                        echo '<div class="card col-sm-12 col-md-6 col-lg-3 m-1">';
+                                                        echo '<h3 class="card-title" style="width: 100%; font-size:25px;">' . ucwords($reg3['marca']) . '</h3>';
+                                                        echo '<span>$ ' . $reg3['precio'] . '</span>';
+                                                        echo '</div>';
+                                                    }
+                                                }
+                                                // Ejercicio 2 parte d
+                                                echo '<div class="mt-2 subtitle-1">
+                                                <div class="mt-2 ml-2 subtitle-1">d. Listar todos los registros cuyo precio sea mayor a ‘500’</div>
+                                                </div>';
+                                                $datos4 = mysqli_query($conexion, $consulta);
+                                                while ($reg4 = mysqli_fetch_array($datos4)) {
+                                                    if ($reg4['precio'] > 500) {
+                                                        echo '<div class="card col-sm-12 col-md-6 col-lg-3 m-1">';
+                                                        echo '<h3 class="card-title" style="width: 100%; font-size:25px;">' . ucwords($reg4['marca']) . '</h3>';
+                                                        echo '<span>$ ' . $reg4['precio'] . '</span>';
+                                                        echo '</div>';
+                                                    }
+                                                }
+                                                echo '</div>';
+                                                ?>
                                             </div>
                                         </div>
                                     </section>
